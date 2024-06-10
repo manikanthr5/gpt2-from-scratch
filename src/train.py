1b9f7eef981f31f5252c89183826234c4d17e73d
Manikanth Reddy <manikanth.iitkgp@gmail.com>
2024-06-10 09:49:12 +0530

    implement causal self attention

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,6 +14,91 @@ class GPTConfig:
     n_embed: int = 384
 
 
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config) -> None:
+        super().__init__()
+        assert config.n_embed % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embed, 3 * config.n_embed)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embed, config.n_embed)
+        self.n_head = config.n_head
+        self.n_embed = config.n_embed
+        # not really a 'bias', more of a mask, but following the OpenAI/HF naming though
+        self.register_buffer(
+            "bias",
+            torch.tril(
+                torch.ones(config.block_size, config.block_size).view(
+                    1, 1, config.block_size, config.block_size
+                )
+            ),
+        )
+
+    def forward(self, x):
+        B, T, C = x.size()  # batch size, sequence length, embedding dimension (n_embed)
+        # calculate query, key, values for all head in the batch
+        # and move head forward to the batch dimension
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * ns
+        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs = C = 768 channels in the transformer
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embed, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(
+            1, 2
+        )  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # ...
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # ...
+        # attention (materializes the large (T, T) matrix for all the queries and keys)
+        attn = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        attn = attn.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+        attn = F.softmax(attn, dim=-1)
+        y = attn @ v  # (B, nh, T, T) * (B, nh, T, hs) -> (B, nh, T, hs)
+        y = (
+            y.transpose(1, 2).contiguous().view(B, T, C)
+        )  # re-assemble all head outputs side by side
+        # output projection
+        y = self.c_proj(y)
+        return y
+
+
+class MLP(nn.Module):
+    def __init__(self, config) -> None:
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embed, 4 * config.n_embed)
+        self.gelu = nn.GELU(approximate="tanh")
+        self.c_proj = nn.Linear(4 * config.n_embed, config.n_embed)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self, config) -> None:
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embed)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embed)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
 class GPT(nn.Module):
     def __init__(self, config) -> None:
+        super().__init__()
         self.config = config
+
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(config.vocab_size, config.n_embed),
+                wpe=nn.Embedding(config.block_size, config.n_embed),
+                h=nn.Module([Block(config) for _ in range(config.n_layer)]),
+                ln_f=nn.LayerNorm(config.n_embed),
+            )
+        )
+        self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
