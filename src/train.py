bfbfa7f44229e39ccea255aafb43348e106a4fa3
Manikanth Reddy <manikanth.iitkgp@gmail.com>
2024-06-10 09:14:42 +0530

    check huggingface gpt 2 implementation

---
+++ src/train.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+@dataclass
+class GPTConfig:
+    block_size: int = 256
+    vocab_size: int = 65
+    n_layer: int = 6
+    n_head: int = 6
+    n_embed: int = 384
+
+
+class GPT(nn.Module):
+    def __init__(self, config) -> None:
+        self.config = config
